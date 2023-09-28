cc1f8ce79566ed687f4ed68f8c29cf727cb43770
VolemAzira <volemalvaroazira2@gmail.com>
2023-09-28 14:32:45 +0700

    es6

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
-const Projects = require("./models/Projects.js");
+import Projects from "./models/Projects.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
